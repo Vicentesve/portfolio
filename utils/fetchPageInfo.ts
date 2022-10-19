e1561b400a2d83ab1b1ee936af176c6bf2fe1309
Vicente Santamaría Velasco <vicente.40080@gmail.com>
2022-10-18 20:38:03 -0500

    change fetchPageInfo

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -5,12 +5,8 @@ export const fetchPageInfo = async () => {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
 
-  try {
-    const data = await res.json();
-    const pageInfo: PageInfo = data.pageInfo;
-    return pageInfo;
-  } catch (err) {
-    console.log("err: ", err);
-    return err;
-  }
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
+
+  return pageInfo;
 };
