82054cd4059fb1b8bd1628c87d50beecdda604b6
Vicente Santamaría Velasco <vicente.40080@gmail.com>
2022-10-18 20:23:07 -0500

    Change

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -5,14 +5,7 @@ export const fetchPageInfo = async () => {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
 
-  console.log(res.status);
-
-  if (res.status !== 200) {
-    throw new Error(`There was an error with status code ${res.status}`);
-  }
-
   const data = await res.json();
-
   const pageInfo: PageInfo = data.pageInfo;
 
   return pageInfo;
